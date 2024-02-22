018d7a98881a1456cf86fa8e8c6717ce32a3254a
Hp <Hp@LAPTOP-E4JLOJEE>
2024-02-22 12:21:02 +0530

    Repository and Service layer interfaces

---
+++ src/main/java/com/example/restdemo/repository/CloudVendorRepository.java
@@ -0,0 +1,11 @@
+package com.example.restdemo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.restdemo.model.CloudVendor;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String>{
+
+//JpaRepository provides methods to save in db
+
+}
---
+++ src/main/java/com/example/restdemo/service/CloudVendorService.java
@@ -0,0 +1,16 @@
+package com.example.restdemo.service;
+
+import java.util.List;
+
+import com.example.restdemo.model.CloudVendor;
+
+public interface CloudVendorService {
+
+	//This is my business/service layer
+	
+	public String createCloudVendor(CloudVendor cloudVendor);
+	public String updateCloudVendor(CloudVendor cloudVendor);
+	public String deleteCloudVendor(String cloudVendorId);
+	public CloudVendor getCloudVendor(String cloudVendorId);
+	public List<CloudVendor> getAllCloudVendor();
+}
